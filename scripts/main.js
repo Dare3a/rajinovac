920392bf07a3f14b4a71c87f6f7b9fd751f4a3f5
darko2019 <darko.ilic83@gmail.com>
2022-07-23 10:57:54 +0200

    Ubaceno otvaranje Navigacije na hover

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
+let dropDownTab = document.querySelector(".dropdown");
+let dropDownTabClicable = document.querySelector("#navbarDropdownMenuLink");
+let dropDownMenu = document.querySelector(".dropdown-menu");
+// Otvaranje dropdown menija u navigaciji onhover
+if (dropDownTabClicable) {
+    dropDownTab.addEventListener("mouseenter", () => {
+        dropDownMenu.classList.add("show");
+        console.log("radim");
+    });
+}
+// Zatvaranje dropdown menija u navigaciji na izlaz misa
+if (dropDownMenu) {
+    let dropDownList = [dropDownTab];
+    dropDownList.forEach((element) => {
+        element.addEventListener("mouseleave", () => {
+            dropDownMenu.classList.remove("show");
+            console.log("radim na izlas");
+        });
+    });
+}
 /*back to top*/
 $("#back-to-top").hide();
 $(document).ready(function () {
