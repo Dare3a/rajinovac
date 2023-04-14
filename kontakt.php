90ef22627efa05a54b83fa91388b5582d3d85cbb
darko2019 <darko.ilic83@gmail.com>
2023-04-14 17:11:35 +0200

    U php za kontakt formu ubaceno prezime

--- kontakt.php
+++ kontakt.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["submit"])) {
     $name = $_POST['name'];
-    $company = $_POST['company'];
+    $surname = $_POST['surname'];
     $email = $_POST['email'];
     $telefon = $_POST['telefon'];
     $message = $_POST['message'];
@@ -9,7 +9,7 @@ if (isset($_POST["submit"])) {
 
     $to = 'darko.ilic83@gmail.com';
     $subject = 'Contact form';
-    $body = "Ime: $name\r\n\nKompanija: $company\n\nE-mail: $email\n\nTelefon: $telefon\n\nPoruka: $message";
+    $body = "Ime: $name\r\n\nPrezime: $surname\n\nE-mail: $email\n\nTelefon: $telefon\n\nPoruka: $message";
 
 // More headers
     $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
