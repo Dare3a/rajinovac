4c95db43b93bbbff4a66a2f0a67aa6ebe7e9663d
darko2019 <darko.ilic83@gmail.com>
2023-05-16 00:53:04 +0200

    posle testiranja ubacena email adresa manastirrajinovac@gmail.com

--- kontakt.php
+++ kontakt.php
@@ -7,7 +7,7 @@ if (isset($_POST["submit"])) {
     $message = $_POST['message'];
 
 
-    $to = 'darko.ilic83@gmail.com';
+    $to = 'manastirrajinovac@gmail.com';
     $subject = 'Contact form';
     $body = "Ime: $name\r\n\nPrezime: $surname\n\nE-mail: $email\n\nTelefon: $telefon\n\nPoruka: $message";
 
